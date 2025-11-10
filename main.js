fa8777002e92879149348f0bb35ab6a2a14345f6
devrishisahu <rishisahu9695@gmail.com>
2025-11-11 03:07:29 +0530

    snake-game

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ const currencyCode = document.querySelector('#currencyCode')
 const currencyCodeTwo = document.querySelector('#currencyCodeTwo')
 const currencyDisplayTwo = document.querySelector('#currencyDisplayTwo')
 const currencyDisplay = document.querySelector('#currencyDisplay')
+const hideBtn = document.querySelector('#hideBtn')
+const gameDisplay = document.querySelector('#gameDisplay')
 let button = document.querySelectorAll('button')
+let calculatorDisplay = document.querySelector('#calculatorDisplay')
+let gameScreen = document.querySelector('#gameScreen')
 
 
 function appendToDisplay(input) {
@@ -64,10 +68,14 @@ function changeDisplay() {
     currencyDisplayTwo.value = currencyDisplayTwo.popover
 
     if (changer.checked) {
+        hideBtn.className = "bg-lime-300 h-15 w-15 rounded-4xl  hover:bg-lime-200 active:bg-lime-100 text-xl font-semibold hidden"
+        gameDisplay.className = "bg-lime-300 h-15 w-15 rounded-4xl  hover:bg-lime-200 active:bg-lime-100 text-xl font-semibold "
         display.className = " border-b-2 my-2 border-stone-300 pr-2 h-30 outline-none items-end text-4xl text-end text-ellipsis hidden"
         displayTwo.className = "border-b-2 my-2 border-stone-300 pr-2 pb-3 h-30 justify-end outline-none flex flex-col text-end text-ellipsis  "
 
     } else {
+        hideBtn.className = "bg-lime-300 h-15 w-15 rounded-4xl  hover:bg-lime-200 active:bg-lime-100 text-xl font-semibold "
+        gameDisplay.className = "bg-lime-300 h-15 w-15 rounded-4xl  hover:bg-lime-200 active:bg-lime-100 text-xl font-semibold hidden "
         display.className = " border-b-2 my-2 border-stone-300 pr-2 h-30 outline-none items-end text-4xl text-end text-ellipsis "
         displayTwo.className = "border-b-2 my-2 border-stone-300 pr-2 pb-3 h-30 justify-end outline-none flex flex-col text-end text-ellipsis hidden "
     }
@@ -83,5 +91,39 @@ const currencyConverter = async (e) => {
     let code = data.exchange_rates[liveCode]
     const results = currencyDisplay.value * code
     currencyDisplayTwo.value += results.toFixed(2)
+}
+
+
+const snakeGame = () => {
+
+    calculatorDisplay.className = " hidden "
+    gameScreen.className = "flex flex-col min-h-120 w-90 rounded-2xl bg-lime-100 border-3 py-2 border-stone-300 shadow-2xl "
+
+}
+
+function returnCurrDisplay() {
+
+    calculatorDisplay.className = " flex gap-2 flex-col min-h-120 w-90 rounded-2xl p-1 bg-lime-100 border-3  border-stone-300 shadow-2xl overflow-hidden "
+    gameScreen.className = "flex flex-col min-h-120 w-90 rounded-2xl bg-lime-100 border-3 py-2 border-stone-300 shadow-2xl hidden "
+
+    hideBtn.className = "bg-lime-300 h-15 w-15 rounded-4xl  hover:bg-lime-200 active:bg-lime-100 text-xl font-semibold hidden"
+    gameDisplay.className = "bg-lime-300 h-15 w-15 rounded-4xl  hover:bg-lime-200 active:bg-lime-100 text-xl font-semibold "
+
+    display.className = " border-b-2 my-2 border-stone-300 pr-2 h-30 outline-none items-end text-4xl text-end text-ellipsis hidden"
+    displayTwo.className = "border-b-2 my-2 border-stone-300 pr-2 pb-3 h-30 justify-end outline-none flex flex-col text-end text-ellipsis  "
+
+}
+
+function returnCalcDisplay() {
+
+    calculatorDisplay.className = " flex gap-2 flex-col min-h-120 w-90 rounded-2xl p-1 bg-lime-100 border-3  border-stone-300 shadow-2xl overflow-hidden "
+    gameScreen.className = "flex flex-col min-h-120 w-90 rounded-2xl bg-lime-100 border-3 py-2 border-stone-300 shadow-2xl hidden "
+
+    hideBtn.className = "bg-lime-300 h-15 w-15 rounded-4xl  hover:bg-lime-200 active:bg-lime-100 text-xl font-semibold "
+    gameDisplay.className = "bg-lime-300 h-15 w-15 rounded-4xl  hover:bg-lime-200 active:bg-lime-100 text-xl font-semibold hidden "
+
+    display.className = " border-b-2 my-2 border-stone-300 pr-2 h-30 outline-none items-end text-4xl text-end text-ellipsis "
+    displayTwo.className = "border-b-2 my-2 border-stone-300 pr-2 pb-3 h-30 justify-end outline-none flex flex-col text-end text-ellipsis hidden "
+
 }
 
